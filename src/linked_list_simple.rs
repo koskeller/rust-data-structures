2c0573668f8f3f2a62e2e0a5164297d0a87a6d57
Konstantin Keller <koskeller@gmail.com>
2023-04-16 15:48:28 +0300

    feat: IterMut implementation for linked list

--- src/linked_list_simple.rs
+++ src/linked_list_simple.rs
@@ -64,6 +64,12 @@ impl<T> List<T> {
             next: self.head.as_deref(),
         }
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
 }
 
 pub struct IntoIter<T>(List<T>);
@@ -85,6 +91,21 @@ impl<'a, T> Iterator for Iter<'a, T> {
         self.next.map(|node| {
             self.next = node.next.as_deref();
             &node.value
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.value
         })
     }
 }
@@ -183,5 +204,18 @@ mod test {
         assert_eq!(iter.next(), None);
 
         assert_eq!(list.pop(), Some(3));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
