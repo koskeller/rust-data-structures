ac0a12ee5054e781712dc54bdb631da80d9cc29e
Konstantin Keller <koskeller@gmail.com>
2023-05-13 22:04:24 +0300

    feat: doubly linked deque, push_back() and pop_back() methods

--- src/doubly_linked_deque.rs
+++ src/doubly_linked_deque.rs
@@ -40,6 +40,28 @@ impl<T> List<T> {
         }
     }
 
+    pub fn push_back(&mut self, value: T) {
+        // create new node
+        // check if tail exist, if so
+        //    - replase list tail with new node
+        //    - old node next will link to new node
+        //    - new node prev will link to old node
+        // if not
+        //     - link head and tail to new node
+        let new_node = Node::new(value);
+        match self.tail.take() {
+            Some(old_node) => {
+                old_node.borrow_mut().next = Some(new_node.clone());
+                new_node.borrow_mut().prev = Some(old_node);
+                self.tail = Some(new_node);
+            }
+            None => {
+                self.tail = Some(new_node.clone());
+                self.head = Some(new_node);
+            }
+        }
+    }
+
     pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|old_head| {
             match old_head.borrow_mut().next.take() {
@@ -52,6 +74,21 @@ impl<T> List<T> {
                 }
             }
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().value
+        })
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().value
         })
     }
 
@@ -89,26 +126,43 @@ mod test {
     use super::List;
 
     #[test]
-    fn basic() {
+    fn basic_front() {
         let mut list = List::new();
-
         assert_eq!(list.pop_front(), None);
 
         list.push_front(1);
         list.push_front(2);
         list.push_front(3);
-
         assert_eq!(list.pop_front(), Some(3));
         assert_eq!(list.pop_front(), Some(2));
 
         list.push_front(4);
         list.push_front(5);
-
         assert_eq!(list.pop_front(), Some(5));
         assert_eq!(list.pop_front(), Some(4));
 
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn basic_back() {
+        let mut list = List::new();
+        assert_eq!(list.pop_back(), None);
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        list.push_back(4);
+        list.push_back(5);
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
     }
 
     #[test]
