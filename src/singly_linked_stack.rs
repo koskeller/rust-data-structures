6b1373419c9faf135de18bd73f98894a1d9b1484
Konstantin Keller <koskeller@gmail.com>
2023-05-13 12:03:28 +0300

    feat: single linked stack iter()

--- src/singly_linked_stack.rs
+++ src/singly_linked_stack.rs
@@ -35,6 +35,27 @@ impl<T> List<T> {
             head: self.head.as_ref().and_then(|node| node.next.clone()),
         }
     }
+
+    fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+}
+
+struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.value
+        })
+    }
 }
 
 #[cfg(test)]
@@ -60,5 +81,15 @@ mod test {
 
         let list = list.tail();
         assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
     }
 }
